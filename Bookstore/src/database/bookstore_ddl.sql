481f9f5a9a942886d46746c97c6270cb38f5c62c
Khaled Abdelfattah <khaledabdelfattah74@gmail.com>
2019-05-06 15:33:53 +0200

    Modifying foreign key constraints

--- Bookstore/src/database/bookstore_ddl.sql
+++ Bookstore/src/database/bookstore_ddl.sql
@@ -13,16 +13,20 @@ create table if not exists publisher_addresses
 (
   publisher_name varchar(45) not null,
   address        varchar(45) not null,
+
   primary key (publisher_name, address),
-  constraint publisher_address_fk foreign key (publisher_name) references publisher (publisher_name)
+  constraint publisher_address_fk
+    foreign key (publisher_name) references publisher (publisher_name) on update cascade on delete cascade
 );
 
 create table if not exists publisher_phones
 (
   publisher_name varchar(45) not null,
   phone          varchar(20) not null,
+
   primary key (publisher_name, phone),
-  constraint publisher_phone_fk foreign key (publisher_name) references publisher (publisher_name)
+  constraint publisher_phone_fk
+    foreign key (publisher_name) references publisher (publisher_name) on update cascade on delete cascade
 );
 
 create table if not exists category
@@ -41,10 +45,12 @@ create table if not exists book
   required_quantity integer default 0,
   category_name     varchar(30),
   publisher_name    varchar(45),
+
   constraint category_fk foreign key (category_name) references category (category_name),
   constraint publisher_fk foreign key (publisher_name) references publisher (publisher_name),
   constraint check_quantity check (quantity >= 0),
   constraint check_required_quantity check (required_quantity >= threshold),
+
   index isbn_index (ISBN),
   index title_index (title),
   index category_index (category_name)
@@ -54,8 +60,10 @@ create table if not exists book_authors
 (
   ISBN        integer     not null,
   author_name varchar(45) not null,
+
   primary key (ISBN, author_name),
-  constraint book_author_fk foreign key (ISBN) references book (ISBN)
+  constraint book_author_fk
+    foreign key (ISBN) references book (ISBN) on update cascade on delete cascade
 );
 
 create table if not exists library_orders
@@ -65,7 +73,10 @@ create table if not exists library_orders
   quantity     integer default 0,
   ordered_date date,
   confirmed    bit     default 0,
-  constraint book_order_fk foreign key (ISBN) references book (ISBN),
+
+  constraint book_order_fk
+    foreign key (ISBN) references book (ISBN) on update cascade on delete cascade,
+
   index isbn_index (ISBN)
 );
 
@@ -79,6 +90,7 @@ create table if not exists user
   last_name  varchar(20),
   address    varchar(45),
   is_manger  bit default 0,
+
   primary key (user_name, email)
 );
 
@@ -89,9 +101,13 @@ create table if not exists user_orders
   email          varchar(45) not null,
   quantity       integer default 0,
   check_out_date date,
+
   primary key (ISBN, user_name, email),
-  constraint ordered_book_fk foreign key (ISBN) references book (ISBN),
-  constraint user_fk foreign key (user_name, email) references user (user_name, email),
+  constraint ordered_book_fk
+    foreign key (ISBN) references book (ISBN) on update cascade on delete cascade,
+  constraint user_fk
+    foreign key (user_name, email) references user (user_name, email) on update cascade on delete cascade,
+
   index isbn_index (ISBN),
   index user_index (user_name)
 );
