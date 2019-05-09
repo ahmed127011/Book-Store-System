be0172be0f489ac36d14dec810db5fdf0e02aac5
Ayman <ayman.a.samy.m@gmail.com>
2019-05-09 15:45:49 +0200

    Login And SignUp is functioning correctly

--- Bookstore/src/viewsControllers/Login.java
+++ Bookstore/src/viewsControllers/Login.java
@@ -1,12 +1,19 @@
 package viewsControllers;
 
+import database.DatabaseHandler;
+import database.MysqlDatabaseHandler;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.DialogEvent;
 import javafx.scene.control.TextField;
+import models.LoggedUser;
+import models.User;
 import sample.Main;
 
 import java.io.File;
@@ -18,16 +25,34 @@ import java.util.ResourceBundle;
 public class Login implements Initializable {
     @FXML
     private TextField userNameTxtField;
-
     @FXML
     private TextField passwordTxtField;
 
     public void loginClk(ActionEvent actionEvent) {
-        System.out.println(userNameTxtField.getText());
-        System.out.println(passwordTxtField.getText());
+        DatabaseHandler databaseHandler = new MysqlDatabaseHandler();
+        String username = userNameTxtField.getText();
+        String password = passwordTxtField.getText();
+        Boolean loggedin = databaseHandler.login(username,password);
+        if(loggedin) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Successfully Signed In");
+            alert.setOnCloseRequest(new EventHandler<DialogEvent>() {
+                @Override
+                public void handle(DialogEvent dialogEvent) {
+                    if(LoggedUser.getInstance().getUser().getIsManger()) {
+                        // Todo : go to manager Home page
+                    } else  {
+                        // Todo : go to user Home page
+                    }
+                }
+            });
+            alert.show();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Wrong login Information");
+            alert.show();
+        }
     }
 
-    public void SignUpClk(ActionEvent actionEvent) throws IOException {
+    public void SignUpClk(ActionEvent actionEvent) {
         ViewsController.getInstance().openSignUpScreen();
     }
 
--- Bookstore/src/viewsControllers/SignUp.java
+++ Bookstore/src/viewsControllers/SignUp.java
@@ -1,26 +1,73 @@
 package viewsControllers;
 
 
+import database.DatabaseHandler;
+import database.MysqlDatabaseHandler;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import sample.Main;
 
-import java.io.File;
-import java.io.IOException;
+import javafx.scene.control.Alert;
+import javafx.scene.control.DialogEvent;
+import javafx.scene.control.TextField;
+import models.User;
+
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SignUp implements Initializable {
+
+    @FXML
+    private TextField userNameTxtField;
+    @FXML
+    private TextField passwordTxtField;
+    @FXML
+    private TextField emailTxtField;
+    @FXML
+    private TextField firstNameTxtField;
+    @FXML
+    private TextField lastNameTxtField;
+    @FXML
+    private TextField addressTxtField;
+    @FXML
+    private TextField phoneTxtField;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
 
     public void SignUpClk(ActionEvent actionEvent) {
+        DatabaseHandler databaseHandler = new MysqlDatabaseHandler();
+        String username = userNameTxtField.getText();
+        String password = passwordTxtField.getText();
+        String email = emailTxtField.getText();
+        String phone = phoneTxtField.getText();
+        String firstName = firstNameTxtField.getText();
+        String lastName = lastNameTxtField.getText();
+        String address = addressTxtField.getText();
 
+        User user = new User(username, email, password, phone);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setAddress(address);
+
+        Boolean signedUp = databaseHandler.signUp(user);
+        if(signedUp) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Successfully Signed Up");
+            alert.setOnCloseRequest(new EventHandler<DialogEvent>() {
+                @Override
+                public void handle(DialogEvent dialogEvent) {
+                    ViewsController.getInstance().openLoginScreen();
+                }
+            });
+            alert.show();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Wrong SignUp Information");
+            alert.show();
+        }
     }
 
     public void backClk(ActionEvent actionEvent) {
