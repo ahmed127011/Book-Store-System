6b45030a49a075334722bd44af98d5b3e5d7322c
Ayman <ayman.a.samy.m@gmail.com>
2019-05-13 00:29:38 +0200

    Finish Report

--- Bookstore/src/viewsControllers/ControlPanel.java
+++ Bookstore/src/viewsControllers/ControlPanel.java
@@ -5,10 +5,16 @@ import database.MysqlDatabaseHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextInputDialog;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import models.LoggedUser;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -20,8 +26,35 @@ public class ControlPanel implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) { }
 
-    public void generateReportClk(ActionEvent actionEvent) {
-        // Todo : generate reports
+    private void generateAlert(String title,String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(title);
+
+        // Create expandable Exception.
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+
+
+        TextArea textArea = new TextArea(message);
+        textArea.setEditable(false);
+        textArea.setWrapText(true);
+
+        textArea.setMaxWidth(Double.MAX_VALUE);
+        textArea.setMaxHeight(Double.MAX_VALUE);
+        GridPane.setVgrow(textArea, Priority.ALWAYS);
+        GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+        GridPane alertContent = new GridPane();
+        alertContent.setMaxWidth(Double.MAX_VALUE);
+        alertContent.add(textArea, 0, 0);
+
+        // Set expandable Exception into the dialog pane.
+        alert.getDialogPane().setExpandableContent(alertContent);
+
+        alert.showAndWait();
+
     }
 
     public void searchBooksClk(ActionEvent actionEvent) throws IOException {
@@ -68,5 +101,21 @@ public class ControlPanel implements Initializable {
 
     public void addPublisherClk(ActionEvent actionEvent) throws IOException {
         ViewsController.getInstance().openAddPublisherScreen();
+    }
+
+    public void totlalSalesClk(ActionEvent actionEvent) {
+        DatabaseHandler databaseHandler = MysqlDatabaseHandler.getInstance();
+        generateAlert("The total sales for books in the previous month", String.valueOf(databaseHandler.getprevMonthSales()));
+    }
+
+    public void topCustomersClk(ActionEvent actionEvent) {
+        DatabaseHandler databaseHandler = MysqlDatabaseHandler.getInstance();
+        generateAlert("The top 5 customers who purchase the most purchase amount in descending order for the last\n" +
+                "three months", databaseHandler.getTop5Customers());
+    }
+
+    public void topBooksClk(ActionEvent actionEvent) {
+        DatabaseHandler databaseHandler = MysqlDatabaseHandler.getInstance();
+        generateAlert("The top 10 selling books for the last three months", databaseHandler.viewTopSellingBooks());
     }
 }
